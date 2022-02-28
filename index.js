52d6dca58d0dba485adbcb299170b1c1c3f988ff
Baltazar-Figuereo <Bfiguereo40@gmail.com>
2022-02-28 14:44:36 -0500

    Added /friend and /message url endpoints

--- index.js
+++ index.js
@@ -13,17 +13,36 @@ const config = {
 const PORT = 3000;
 
 // both req and res are readable and writable streams
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Context-Type': "application/json",
-    });
+// resquest is an event emitter.
 
-    // need to close the writable stream at the end.
-    // res.end("Hello! Sir Isaax Newton is your friend!");
-    res.end(JSON.stringify({
-            id: 1,
-            name: "Sir Issac Newton"
-    }));
+// this is similar to server.on('request') event emitter.
+const server = http.createServer((req, res) => {
+    if(req.url === "/friends") {
+        /*
+        res.writeHead(200, {
+            'Content-Type': "application/json",
+        });
+        */
+
+        res.statusCode = 200
+        res.setHeader("Content-Type","application/json")
+    
+        // need to close the writable stream at the end.
+        // res.end("Hello! Sir Isaax Newton is your friend!");
+        res.end(JSON.stringify({
+                id: 1,
+                name: "Sir Issac Newton"
+        }));
+    }
+    else if (req.url === "/messages") {
+        res.write("<html></html>")
+        res.write("<body>")
+        res.write("<span>What are you thought on Astronomy</span>")
+        res.write("</body>")
+        res.write("</html>")
+        res.end();
+    }
+    
 });
 
 // 127.0.01 or localhost
