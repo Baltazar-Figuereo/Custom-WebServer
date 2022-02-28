06cca6a6a968e99a16a5571f8fabf7dc0b191a94
Baltazar-Figuereo <Bfiguereo40@gmail.com>
2022-02-28 11:45:58 -0500

    Added JSON response

--- index.js
+++ index.js
@@ -10,9 +10,23 @@ const config = {
     proxy
 }
 
+const PORT = 3000;
 
-// both req and res are streams
+// both req and res are readable and writable streams
 const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Context-Type': "application/json",
+    });
 
+    // need to close the writable stream at the end.
+    // res.end("Hello! Sir Isaax Newton is your friend!");
+    res.end(JSON.stringify({
+            id: 1,
+            name: "Sir Issac Newton"
+    }));
 });
 
+// 127.0.01 or localhost
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
